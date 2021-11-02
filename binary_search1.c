d503d71f141436057c5dd0cbd924578f6f4a0915
SrinikaSharma <93469490+SrinikaSharma@users.noreply.github.com>
2021-11-02 23:08:55 +0530

    Binary Search

---
+++ binary_search1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int BinarySearching(int[], int , int);
+int main()
+{
+    int i, key, n, arr[50], position;
+    printf("Enter the Limit of Elements in Array:\t");
+    scanf("%d", &n);
+    printf("Enter %d Elements in Array: \n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter Element To Search:\t");
+    scanf("%d", &key);
+    position = BinarySearching(arr, n, key);
+    if(position == -1)
+    {
+        printf("Element %d Not Found\n", key);
+    }
+    else
+    {
+        printf("Element %d Found at Position %d\n", key, position + 1);
+    }
+    return 0;
+}
+
+int BinarySearching(int arr[], int n, int key)
+{
+    int low = 0, high = n - 1, middle;
+    while(low <= high)
+    {
+        middle = (low + high) / 2;
+        if(key > arr[middle])
+            low = middle + 1;
+        else if(key < arr[middle])
+            high = middle - 1;
+        else
+            return middle;
+    }
+    return -1;
+}
