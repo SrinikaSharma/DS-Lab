0c5b63298c95182a766dd1e65885d907f818b6d0
SrinikaSharma <93469490+SrinikaSharma@users.noreply.github.com>
2021-11-16 18:49:56 +0530

    Code for linear search using recursion

---
+++ Week1/LinearSearchRec.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+int linearSearch(int[], int , int, int);
+int main()
+{
+    int n,arr[10],key,result,i;
+    printf("enter the number of elements : ");
+    scanf("%d",&n);
+    printf("\nenter the elements : ");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("\nEnter the element to find : ");
+    scanf("%d",&key);
+    result = linearSearch(arr,n,key,0);
+    if (result == -1)
+    {
+        printf("\nElement not found");
+    }
+    else
+    {
+        printf("\nElement found at %d ",result + 1);
+    }
+}
+
+int linearSearch(int a[10], int n, int key, int index)
+{
+    if (index >= n)
+    {
+        return 0;
+    }
+    else if(a[index] == key)
+    {
+        return index;
+    }
+    else
+    {
+        return linearSearch(a,n,key,index+1);
+    }
+    return -1;
+}
+/* Output :
+enter the number of elements : 5
+enter the elements : 5 8 9 3 2
+Enter the element to find : 9
+Element found at 3 */
+
+/* Output :
+enter the number of elements : 5
+enter the elements : 5 8 9 3 2
+Enter the element to find : 4
+Element not found */
