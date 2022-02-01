cc925819e45ae005e36fedca3607892d49c9091b
SrinikaSharma <93469490+SrinikaSharma@users.noreply.github.com>
2022-02-01 11:59:22 +0530

    Depth First Search

---
+++ Graph Traversal Methods/DFS.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<conio.h>
+int a[20][20],reach[20],n;
+void dfs(int v) {
+	int i;
+	reach[v]=1;
+	for (i=1;i<=n;i++)
+	  if(a[v][i] && !reach[i]) {
+		printf("\n %d->%d",v,i);
+		dfs(i);
+	}
+}
+void main() 
+{
+	int i,j,count=0;
+		printf("\n Enter number of vertices:");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++) {
+		reach[i]=0;
+		for (j=1;j<=n;j++)
+		   a[i][j]=0;
+	}
+	printf("\n Enter the adjacency matrix:\n");
+	for (i=1;i<=n;i++)
+	  for (j=1;j<=n;j++)
+	   scanf("%d",&a[i][j]);
+	dfs(1);
+	printf("\n");
+	
+}
+
+/* OUTPUT :
+
+
+ Enter number of vertices:5
+
+ Enter the adjacency matrix:
+0 1 1 0 0
+1 0 0 1 0
+1 0 0 0 1
+0 1 0 0 1
+0 0 1 1 0
+
+ 1->2
+ 2->4
+ 4->5
+ 5->3
+
+--------------------------------
+
+
+ Enter number of vertices:8
+
+ Enter the adjacency matrix:
+0 1 1 1 1 0 0 0
+1 0 0 0 0 1 0 0
+1 0 0 0 0 1 0 0
+1 0 0 0 0 0 1 0
+1 0 0 0 0 0 1 0
+0 1 1 0 0 0 0 1
+0 0 0 1 1 0 0 1
+0 0 0 0 0 1 1 0
+
+ 1->2
+ 2->6
+ 6->3
+ 6->8
+ 8->7
+ 7->4
+ 7->5
+
+--------------------------------
+
+*/
